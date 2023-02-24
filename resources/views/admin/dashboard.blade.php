7dec901e9f6b22738e8687dec90dc5094fdf0491
Dipali Jani <77090349+dipali-j-eq@users.noreply.github.com>
2023-02-24 19:04:24 +0530

    created login

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+
+    <style>
+
+    </style>
+    </head>
+    <body>
+  <section>
+    <div class="container">
+      
+    </div>
+  </section>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\CustomAuthController;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
+
+Route::post('/check-login', function (Request $request) {
+    
+    // $data = Request::createFromGlobals()->all();
+    // print_r($data);
+    // return view('auth/login', $data);
+
+    $request->validate([
+        'username' => 'required',
+        'password' => 'required',
+    ]);
+    $recordCount = User::where('username', '=' ,$data['username'])->first();
+    // $credentials = $request->only('username', 'password');
+
+    if ($recordCount) {
+        return redirect("admin/dashbord");
+    }
+    else
+    {   
+        return redirect("/")->withErrors('Login details are not valid');
+    }
+    
+
+
+});
+
+// Route::get('/greeting', function () {
+//     return 'Hello World';
+// });
+
+// Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+// Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+// Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+// Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+// Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+// Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
